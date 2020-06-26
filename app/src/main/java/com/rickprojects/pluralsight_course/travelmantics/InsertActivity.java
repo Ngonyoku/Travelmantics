2c505015ce3dd7f56bcb251e72b21f04936b240b
Ngonyoku <ngonyoku001@gmail.com>
2020-06-26 04:31:52 +0300

    RecyclerView Added

--- app/src/main/java/com/rickprojects/pluralsight_course/travelmantics/InsertActivity.java
+++ app/src/main/java/com/rickprojects/pluralsight_course/travelmantics/InsertActivity.java
@@ -24,8 +24,9 @@ public class InsertActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
 
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
 
         txtTitle = findViewById(R.id.txtTitle);
         txtPrice = findViewById(R.id.txtPrice);
